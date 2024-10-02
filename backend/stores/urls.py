3284302c1f517ccba31624d8d49a596fa13d683c
nasr <ahmedmahmoudnasr765@gmail.com>
2024-10-02 10:10:58 +0300

    add tags to swagger

--- backend/stores/urls.py
+++ backend/stores/urls.py
@@ -17,22 +17,3 @@ sections_router.register(r'templates', views.SectionTemplates, basename='section
 
 
 urlpatterns = router.urls + stores_router.urls + sections_router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns = [
-#     router.urls + stores_router.urls,
-    
-#     path('<int:store_pk>/sections', views.SectionList.as_view()),
-#     path('section/<int:section_pk>/templates', views.SectionTemplatesList.as_view()),
-#     path('section/<int:section_pk>/templates/<int:template_pk>', views.SectionTemplatesRetrive.as_view()),
-# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-# router = DefaultRouter()
-# router.register(r'stores', StoreViewSet, basename='store')
-
-# # إنشاء الراوتر المتداخل للأقسام داخل المتاجر
-# stores_router = NestedDefaultRouter(router, r'stores', lookup='store')
-# stores_router.register(r'sections', SectionViewSet, basename='store-sections')
-
-# # جمع كل الراوترات معًا
-# urlpatterns = router.urls + stores_router.urls
--- backend/stores/views.py
+++ backend/stores/views.py
@@ -1,16 +1,12 @@
 from rest_framework import viewsets, mixins
 from .models import Store, Section, Template
 from .serializers import SectionSerializer , StoreSerializer, SectionTemplatesSerializer
-
-from rest_framework import viewsets, mixins
-from .models import Store, Section, Template
-from .serializers import SectionSerializer, StoreSerializer, SectionTemplatesSerializer
-
+from drf_yasg.utils import swagger_auto_schema
+from django.utils.decorators import method_decorator
 
 class AuthantecatiedStoreOwner:
     def filter_queryset_by_user(self, queryset, user='user'):
         return queryset.filter(**{f"{user}": self.request.user})
-
 
 class StoreList(viewsets.ReadOnlyModelViewSet, AuthantecatiedStoreOwner):
     serializer_class = StoreSerializer
@@ -18,16 +14,20 @@ class StoreList(viewsets.ReadOnlyModelViewSet, AuthantecatiedStoreOwner):
         queryset =Store.objects.all()
         return self.filter_queryset_by_user(queryset, user='user')
 
-
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(tags=['Section']))
+@method_decorator(name='list', decorator=swagger_auto_schema(tags=['Section']))
 class SectionList(viewsets.ReadOnlyModelViewSet, AuthantecatiedStoreOwner):
     serializer_class = SectionSerializer
 
     def get_queryset(self):
-        store_pk = self.kwargs['store_pk']
+        store_pk = self.kwargs.get('store_pk')
         queryset = Section.objects.filter(store_id=store_pk)
         return self.filter_queryset_by_user(queryset, user='store_id__user')
 
-
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(tags=['SectionTemplates'],))
+@method_decorator(name='list', decorator=swagger_auto_schema(tags=['SectionTemplates']))
+@method_decorator(name='update', decorator=swagger_auto_schema(tags=['SectionTemplates']))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(tags=['SectionTemplates']))
 class SectionTemplates(
     viewsets.GenericViewSet,
     mixins.ListModelMixin,
@@ -38,6 +38,6 @@ class SectionTemplates(
     serializer_class = SectionTemplatesSerializer
 
     def get_queryset(self):
-        section_pk = self.kwargs['section_pk']
+        section_pk = self.kwargs.get('section_pk')
         queryset =Template.objects.filter(section_id=section_pk)
         return self.filter_queryset_by_user(queryset, user='section_id__store_id__user')
